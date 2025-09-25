37545ce3db6ad8389ea9b7ab9675d88208b96947
museqiujing <1482866837@qq.com>
2025-09-25 16:16:26 +0800

    添加项目文件。

---
+++ MainWindow.h
@@ -0,0 +1,27 @@
+﻿#pragma once 
+
+#include <QtWidgets/QWidget>
+#include "ui_MainWindow.h"
+#include <QSerialPort> //这是Qt串口类
+#include <QSerialPortInfo> //这是Qt串口信息类
+#include<QMessageBox> //包含消息框头文件
+
+class SerialDebugAssistant : public QWidget
+{
+    Q_OBJECT
+
+public:
+    SerialDebugAssistant(QWidget *parent = nullptr);
+    ~SerialDebugAssistant();
+
+	//私有的槽函数
+private slots:
+	void on_key_Refresh_COM_clicked();  //按键刷新串口
+	void on_key_open_COM_clicked(); //按键打开串口 
+	void on_key_COM_Tx_clicked(); //按键发送数据
+	void on_key_COM_Tx1_clicked(); //按键清除发送区域的数据
+	void RX_FUN(); //串口接收数据处理函数
+private:
+    Ui::SerialDebugAssistantClass ui;
+};
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include "MainWindow.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    SerialDebugAssistant window;
+    window.show();
+    return app.exec();
+}
